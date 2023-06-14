f4005f6e99b5c548fdf1522e4b6118fc41b31ef7
KyleTownsend74 <55927311+KyleTownsend74@users.noreply.github.com>
2023-06-14 10:07:33 -0700

    Add recipe API URL

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,23 @@ function App() {
 
   async function testReq() {
     try {
-      const response = await axios.get('https://www.boredapi.com/api/activity/');
+      const response = await axios.get('https://api.edamam.com/api/recipes/v2', {
+        params: {
+          type: "public",
+          q: "chicken",
+          app_id: import.meta.env.VITE_APP_ID,
+          app_key: import.meta.env.VITE_APP_KEY
+        }
+      });
       console.log(response);
     } catch (error) {
       console.error(error);
     }
   }
 
-  testReq();
-  console.log("ID: " + import.meta.env.VITE_APP_ID);
-  console.log("Key: " + import.meta.env.VITE_APP_KEY);
+  // testReq();
+  // console.log("ID: " + import.meta.env.VITE_APP_ID);
+  // console.log("Key: " + import.meta.env.VITE_APP_KEY);
 
   return (
     <>
