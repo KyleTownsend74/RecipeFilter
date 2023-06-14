5c6881539a3ffdcd70d749954c2dea47db561e87
KyleTownsend74 <55927311+KyleTownsend74@users.noreply.github.com>
2023-06-14 12:48:24 -0700

    Change indent size

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,27 @@ import Header from './components/Header.jsx'
 import './App.css'
 
 function App() {
-  async function testReq() {
-    try {
-      const response = await axios.get('https://api.edamam.com/api/recipes/v2', {
-        params: {
-          type: "public",
-          q: "chicken",
-          app_id: import.meta.env.VITE_APP_ID,
-          app_key: import.meta.env.VITE_APP_KEY
+    async function testReq() {
+        try {
+            const response = await axios.get('https://api.edamam.com/api/recipes/v2', {
+                params: {
+                    type: "public",
+                    q: "chicken",
+                    app_id: import.meta.env.VITE_APP_ID,
+                    app_key: import.meta.env.VITE_APP_KEY
+                }
+            });
+            console.log(response);
+        } catch (error) {
+            console.error(error);
         }
-      });
-      console.log(response);
-    } catch (error) {
-      console.error(error);
     }
-  }
 
-  // testReq();
+    // testReq();
 
-  return (
-    <Header/>
-  )
+    return (
+        <Header/>
+    )
 }
 
 export default App
