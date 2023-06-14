1a013953997191932873020120cde4d720238030
KyleTownsend74 <55927311+KyleTownsend74@users.noreply.github.com>
2023-06-14 13:05:09 -0700

    Convert to double quotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-import axios from 'axios'
-import Header from './components/Header.jsx'
-import './App.css'
+import axios from "axios"
+import Header from "./components/Header.jsx"
+import "./App.css"
 
 function App() {
     async function testReq() {
         try {
-            const response = await axios.get('https://api.edamam.com/api/recipes/v2', {
+            const response = await axios.get("https://api.edamam.com/api/recipes/v2", {
                 params: {
                     type: "public",
                     q: "chicken",
