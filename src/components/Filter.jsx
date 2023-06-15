330321e768d495d7cbc79ae40bb6a8e8550e52e0
KyleTownsend74 <55927311+KyleTownsend74@users.noreply.github.com>
2023-06-15 10:17:00 -0700

    Add buttons to filter row

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,7 +2,15 @@ import "./Filter.css"
 
 function Filter() {
     return (
-        <div id="filter-box"></div>
+        <div id="filter-box">
+            <form>
+                <button type="button">Allergy/Diet Filter</button>
+                <button type="button">Calories Filter</button>
+                <button type="button">Meal Type</button>
+                <button type="button">Cook/Prep Time</button>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
 
