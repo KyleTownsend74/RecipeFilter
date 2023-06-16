e7256359879a25feeafec25d5143dbf0b1b13b63
KyleTownsend74 <55927311+KyleTownsend74@users.noreply.github.com>
2023-06-16 10:32:31 -0700

    Refactor allergy list and detect checkbox

--- src/components/FilterModals/AllergyDietContent.jsx
+++ src/components/FilterModals/AllergyDietContent.jsx
@@ -1,144 +1,158 @@
 import "./AllergyDietContent.css";
 
 function AllergyDietContent() {
+    const allergyDietList = [
+        {
+            id: "alcohol-free",
+            displayName: "Alcohol Free"
+        },
+        {
+            id: "celery-free",
+            displayName: "Celery Free"
+        },
+        {
+            id: "crustacean-free",
+            displayName: "Crustacean Free"
+        },
+        {
+            id: "dairy-free",
+            displayName: "Dairy Free"
+        },
+        {
+            id: "DASH",
+            displayName: "DASH"
+        },
+        {
+            id: "egg-free",
+            displayName: "Egg Free"
+        },
+        {
+            id: "fish-free",
+            displayName: "Fish Free"
+        },
+        {
+            id: "fodmap-free",
+            displayName: "Fodmap Free"
+        },
+        {
+            id: "gluten-free",
+            displayName: "Gluten Free"
+        },
+        {
+            id: "immuno-supportive",
+            displayName: "Immuno-Supportive"
+        },
+        {
+            id: "keto-friendly",
+            displayName: "Keto Friendly"
+        },
+        {
+            id: "kidney-friendly",
+            displayName: "Kidney Friendly"
+        },
+        {
+            id: "kosher",
+            displayName: "Kosher"
+        },
+        {
+            id: "low-fat-abs",
+            displayName: "Low Fat"
+        },
+        {
+            id: "low-potassium",
+            displayName: "Low Potassium"
+        },
+        {
+            id: "low-sugar",
+            displayName: "Low Sugar"
+        },
+        {
+            id: "lupine-free",
+            displayName: "Lupine Free"
+        },
+        {
+            id: "mollusk-free",
+            displayName: "Mollusk Free"
+        },
+        {
+            id: "mustard-free",
+            displayName: "Mustard Free"
+        },
+        {
+            id: "no-oil-added",
+            displayName: "No Oil Added"
+        },
+        {
+            id: "paleo",
+            displayName: "Paleo"
+        },
+        {
+            id: "peanut-free",
+            displayName: "Peanut Free"
+        },
+        {
+            id: "pescatarian",
+            displayName: "Pescatarian"
+        },
+        {
+            id: "pork-free",
+            displayName: "Pork Free"
+        },
+        {
+            id: "red-meat-free",
+            displayName: "Red Meat Free"
+        },
+        {
+            id: "sesame-free",
+            displayName: "Sesame Free"
+        },
+        {
+            id: "shellfish-free",
+            displayName: "Shellfish Free"
+        },
+        {
+            id: "soy-free",
+            displayName: "Soy Free"
+        },
+        {
+            id: "sugar-conscious",
+            displayName: "Sugar Conscious"
+        },
+        {
+            id: "sulfite-free",
+            displayName: "Sulfite Free"
+        },
+        {
+            id: "tree-nut-free",
+            displayName: "Tree Nut Free"
+        },
+        {
+            id: "vegan",
+            displayName: "Vegan"
+        },
+        {
+            id: "vegetarian",
+            displayName: "Vegetarian"
+        },
+        {
+            id: "wheat-free",
+            displayName: "Wheat Free"
+        },
+    ];
+    const elements = allergyDietList.map(item => 
+        <div key={item.id} className="input-container">
+            <input onClick={(e) => updateFilter(e)} name={item.id} id={item.id} type="checkbox"/>
+            <label htmlFor={item.id}>{item.displayName}</label>
+        </div>
+    );
+
+    function updateFilter(e) {
+        console.log(e.target.id, e.target.checked);
+    }
+
     return (
         <>
-            <div className="input-container">
-                <input id="alcohol" type="checkbox"/>
-                <label htmlFor="alcohol">Alcohol Free</label>
-            </div>
-            <div className="input-container">
-                <input id="celery" type="checkbox"/>
-                <label htmlFor="celery">Celery Free</label>
-            </div>
-            <div className="input-container">
-                <input id="crustacean" type="checkbox"/>
-                <label htmlFor="crustacean">Crustacean Free</label>
-            </div>
-            <div className="input-container">
-                <input id="dairy" type="checkbox"/>
-                <label htmlFor="dairy">Dairy Free</label>
-            </div>
-            <div className="input-container">
-                <input id="DASH" type="checkbox"/>
-                <label htmlFor="DASH">DASH</label>
-            </div>
-            <div className="input-container">
-                <input id="egg" type="checkbox"/>
-                <label htmlFor="egg">Egg Free</label>
-            </div>
-            <div className="input-container">
-                <input id="fish" type="checkbox"/>
-                <label htmlFor="fish">Fish Free</label>
-            </div>
-            <div className="input-container">
-                <input id="fodmap" type="checkbox"/>
-                <label htmlFor="fodmap">Fodmap Free</label>
-            </div>
-            <div className="input-container">
-                <input id="gluten" type="checkbox"/>
-                <label htmlFor="gluten">Gluten Free</label>
-            </div>
-            <div className="input-container">
-                <input id="immuno" type="checkbox"/>
-                <label htmlFor="immuno">Immuno Supportive</label>
-            </div>
-            <div className="input-container">
-                <input id="keto" type="checkbox"/>
-                <label htmlFor="keto">Keto Friendly</label>
-            </div>
-            <div className="input-container">
-                <input id="kidney" type="checkbox"/>
-                <label htmlFor="kidney">Kidney Friendly</label>
-            </div>
-            <div className="input-container">
-                <input id="kosher" type="checkbox"/>
-                <label htmlFor="kosher">Kosher</label>
-            </div>
-            <div className="input-container">
-                <input id="low-fat" type="checkbox"/>
-                <label htmlFor="low-fat">Low Fat</label>
-            </div>
-            <div className="input-container">
-                <input id="low-potassium" type="checkbox"/>
-                <label htmlFor="low-potassium">Low Potassium</label>
-            </div>
-            <div className="input-container">
-                <input id="low-sugar" type="checkbox"/>
-                <label htmlFor="low-sugar">Low Sugar</label>
-            </div>
-            <div className="input-container">
-                <input id="lupine" type="checkbox"/>
-                <label htmlFor="lupine">Lupine Free</label>
-            </div>
-            <div className="input-container">
-                <input id="mollusk" type="checkbox"/>
-                <label htmlFor="mollusk">Mollusk Free</label>
-            </div>
-            <div className="input-container">
-                <input id="mustard" type="checkbox"/>
-                <label htmlFor="mustard">Mustard Free</label>
-            </div>
-            <div className="input-container">
-                <input id="oil" type="checkbox"/>
-                <label htmlFor="oil">No Oil Added</label>
-            </div>
-            <div className="input-container">
-                <input id="paleo" type="checkbox"/>
-                <label htmlFor="paleo">Paleo</label>
-            </div>
-            <div className="input-container">
-                <input id="peanut" type="checkbox"/>
-                <label htmlFor="peanut">Peanut Free</label>
-            </div>
-            <div className="input-container">
-                <input id="pescatarian" type="checkbox"/>
-                <label htmlFor="pescatarian">Pescatarian</label>
-            </div>
-            <div className="input-container">
-                <input id="pork" type="checkbox"/>
-                <label htmlFor="pork">Pork Free</label>
-            </div>
-            <div className="input-container">
-                <input id="red-meat" type="checkbox"/>
-                <label htmlFor="red-meat">Red Meat Free</label>
-            </div>
-            <div className="input-container">
-                <input id="sesame" type="checkbox"/>
-                <label htmlFor="sesame">Sesame Free</label>
-            </div>
-            <div className="input-container">
-                <input id="shellfish" type="checkbox"/>
-                <label htmlFor="shellfish">Shellfish Free</label>
-            </div>
-            <div className="input-container">
-                <input id="soy" type="checkbox"/>
-                <label htmlFor="soy">Soy Free</label>
-            </div>
-            <div className="input-container">
-                <input id="sugar" type="checkbox"/>
-                <label htmlFor="sugar">Sugar Conscious</label>
-            </div>
-            <div className="input-container">
-                <input id="sulfite" type="checkbox"/>
-                <label htmlFor="sulfite">Sulfite Free</label>
-            </div>
-            <div className="input-container">
-                <input id="tree-nut" type="checkbox"/>
-                <label htmlFor="tree-nut">Tree Nut Free</label>
-            </div>
-            <div className="input-container">
-                <input id="vegan" type="checkbox"/>
-                <label htmlFor="vegan">Vegan</label>
-            </div>
-            <div className="input-container">
-                <input id="vegatarian" type="checkbox"/>
-                <label htmlFor="vegatarian">Vegatarian</label>
-            </div>
-            <div className="input-container">
-                <input id="wheat" type="checkbox"/>
-                <label htmlFor="wheat">Wheat Free</label>
-            </div>
+            {elements}
         </>
     )
 }
