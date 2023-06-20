6af5d75506df6de1fe001a74680b7d7db908023e
KyleTownsend74 <55927311+KyleTownsend74@users.noreply.github.com>
2023-06-20 16:16:57 -0700

    Add message when no recipes are displayed

--- src/components/RecipeContainer.jsx
+++ src/components/RecipeContainer.jsx
@@ -4,6 +4,7 @@ import RecipeItem from "./RecipeItem";
 function RecipeContainer({ recipes }) {
     return (
         <div id="recipe-container">
+            {recipes.length <= 0 ? <p id="recipe-message">No recipes to display.</p> : null}
             {recipes.map(recipe => 
                 <RecipeItem key={recipe.recipe.uri} recipe={recipe.recipe}/>    
             )}
