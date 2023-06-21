f9d57679fbcfa5db176db247db21518b729cc2d6
KyleTownsend74 <55927311+KyleTownsend74@users.noreply.github.com>
2023-06-21 12:54:54 -0700

    Add link to recipe items

--- src/components/RecipeItem.jsx
+++ src/components/RecipeItem.jsx
@@ -3,8 +3,10 @@ import "./RecipeItem.css";
 function RecipeItem({ recipe }) {
     return (
         <div className="recipe-item">
-            <img src={recipe.images.SMALL.url} width="150" height="150"/>
-            <h3>{recipe.label}</h3>
+            <a className="recipe-link" href={recipe.shareAs} target="_blank">
+                <img src={recipe.images.SMALL.url} width="150" height="150"/>
+                <h3>{recipe.label}</h3>
+            </a>
         </div>
     )
 }
